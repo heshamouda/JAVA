7d50acf8600ad0cb341dc7fa1905ea55f9560410
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-05-23 17:27:17 +0200

    edit class to handle checked exceptions

--- exceptions/src/exceptions/flow/FlowTest.java
+++ exceptions/src/exceptions/flow/FlowTest.java
@@ -3,68 +3,68 @@ package exceptions.flow;
 public class FlowTest {
 
 	public static void main(String[] args) {
-		
-		p(3);
+
+		try {
+			p(3);
+		} catch (E3 e) {
+
+			e.printStackTrace();
+		}
 
 	}
-	
-	static void p(int status) {
-		
+
+	static void p(int status) throws E3 {
+
 		try {
 			q(status);
 			System.out.println("p - after q()");
-		}
-		catch(E1 ex) {
+		} catch (E1 ex) {
 			System.out.println("p - " + ex.getMessage());
 		}
 		System.out.println("p - end");
 	}
-	
-	static void q(int status) {
-		
+
+	static void q(int status) throws E1, E3 {
+
 		try {
 			r(status);
 			System.out.println("q - after r()");
-		}
-		catch(E2 ex) {
+		} catch (E2 ex) {
 			System.out.println("q - " + ex.getMessage());
-		}
-		finally {
+		} finally {
 			System.out.println("q - finally...");
 		}
 		System.out.println("q - end");
 	}
-	
-	static void r(int status) {
-		if(status == 0) {
+
+	static void r(int status) throws E1, E2, E3 {
+		if (status == 0) {
 			throw new E1("E1");
-		}
-		else if(status == 1) {
+		} else if (status == 1) {
 			throw new E2("E2");
-		}
-		else if(status == 2) {
+		} else if (status == 2) {
 			throw new E3("E3");
 		}
 	}
 
 }
 
-class E1 extends RuntimeException {
-	
+class E1 extends Exception {
+
 	E1(String msg) {
 		super(msg);
 	}
 }
 
-class E2 extends RuntimeException {
-	
+class E2 extends Exception {
+
 	E2(String msg) {
 		super(msg);
 	}
 }
 
-class E3 extends RuntimeException {
-	
+class E3 extends Exception {
+
 	E3(String msg) {
 		super(msg);
 	}
