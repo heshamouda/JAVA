6ef97c6f06373bf0bd51810c344d0b7968e63ce0
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-05-24 22:06:05 +0200

    creat new class to test person

---
+++ persontest_2/src/test/java/unittesting/PersonTest.java
@@ -0,0 +1,14 @@
+package unittesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void testPerson() {
+		fail("Not yet implemented");
+	}
+
+}
