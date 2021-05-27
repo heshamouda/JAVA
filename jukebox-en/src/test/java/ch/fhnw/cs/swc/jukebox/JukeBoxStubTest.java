e6286948bd7261231e667ec33b1b2d9aac71a52a
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-05-27 12:04:43 +0200

    Task 1:  testing with simple Stubs
    Add an implementation of the stub class SongStub, meaning all methods
    return a hardcoded value.

--- jukebox-en/src/test/java/ch/fhnw/cs/swc/jukebox/JukeBoxStubTest.java
+++ jukebox-en/src/test/java/ch/fhnw/cs/swc/jukebox/JukeBoxStubTest.java
@@ -9,59 +9,77 @@ import org.junit.jupiter.api.Test;
 
 class JukeBoxStubTest {
 
-    protected static String songTitle = "Icebreaker";
-    protected JukeBox jukeBox;
-    protected Song song;
+	protected static String songTitle = "Icebreaker";
+	protected JukeBox jukeBox;
+	protected Song song;
 
-    @BeforeEach
-    public void setUp() {
-        song = new SongStub(songTitle);
-        jukeBox = new MusicJukeBox();
-    }
+	@BeforeEach
+	public void setUp() {
+		song = new SongStub(songTitle);
+		jukeBox = new MusicJukeBox();
+	}
 
-    @Test
-    public void testPlayOfNonExistingSong() {
-        assertThrows(JukeBoxException.class, () -> jukeBox.playSong("not existing"));
-    }
+	/*
+	 * testPlayOfNonExistingSong tries to play a non-existing song. The MusicJukeBox
+	 * should generate an exception in this case and verify the correct exception
+	 * message.
+	 */
+	@Test
+	public void testPlayOfNonExistingSong() {
+		assertThrows(JukeBoxException.class, () -> jukeBox.playSong("not existing"));
+	}
 
-    @Test
-    public void testGetPlayList() {
-        jukeBox.addSong(song);
-        List<Song> list = jukeBox.getPlayList();
-        
-        assertEquals(1, list.size());
-        assertTrue(list.contains(song));
-        
-        // add a second song and test for correct play list
-        Song song2 = new SongStub("Hello Kitty");
-        jukeBox.addSong(song2);
-        list = jukeBox.getPlayList();
-        
-        assertEquals(2, list.size());
-        assertTrue(list.contains(song2));
-    }
+	/*
+	 * testGetPlayList adds two new songs to the MusicJukeBox and checks if the play
+	 * list is not empty and the songs are in the list.
+	 */
+	@Test
+	public void testGetPlayList() {
+		jukeBox.addSong(song);
+		List<Song> list = jukeBox.getPlayList();
 
-    @Test
-    public void testPlaySong() {
-        jukeBox.addSong(song);
-        jukeBox.playSong(songTitle);
-        Song mySong = jukeBox.getCurrentSong();
-        assertTrue(mySong.isPlaying());
-    }
-    
-    @Test
-    public void testPlayOfAlreadyPlayingSong() {
-        jukeBox.addSong(song);
-        
-        // play song; should not generate any exception
-        try {
-          jukeBox.playSong(songTitle);
-        } catch(JukeBoxException e) {
-          fail("no exception expected at first call of playTitle"); 
-        }
-        
-        assertThrows(JukeBoxException.class, () -> jukeBox.playSong(songTitle));
-    }
+		assertEquals(1, list.size());
+		assertTrue(list.contains(song));
 
+		// add a second song and test for correct play list
+		Song song2 = new SongStub2("Hello Kitty");
+		jukeBox.addSong(song2);
+		list = jukeBox.getPlayList();
+
+		assertEquals(2, list.size());
+		assertTrue(list.contains(song2));
+	}
+
+	/*
+	 * testPlaySong adds a new song to the MusicJukeBox and tries to play it. It
+	 * also retrieves the current song and verifies if it is currently played.
+	 */
+	@Test
+	public void testPlaySong() {
+		jukeBox.addSong(song);
+		jukeBox.playSong(songTitle);
+		Song mySong = jukeBox.getCurrentSong();
+		assertTrue(mySong.isPlaying());
+	}
+
+	/*
+	 * testPlayOfAlreadyPlayingSong adds a new song to the MusicJukeBox 
+	 * and tries to start the song two times. 
+	 * The second start of the already playing song
+	 * should generate an exception caused by the Song class. 
+	 */
+	@Test
+	public void testPlayOfAlreadyPlayingSong() {
+		jukeBox.addSong(song);
+
+		// play song; should not generate any exception
+		try {
+			jukeBox.playSong(songTitle);
+		} catch (JukeBoxException e) {
+			fail("no exception expected at first call of playTitle");
+		}
+
+		assertThrows(JukeBoxException.class, () -> jukeBox.playSong(songTitle));
+	}
 
 }
---
+++ jukebox-en/src/test/java/ch/fhnw/cs/swc/jukebox/SongStub2.java
@@ -0,0 +1,33 @@
+package ch.fhnw.cs.swc.jukebox;
+
+public class SongStub2 implements Song {
+	
+	public SongStub2(String title) { 
+		 
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return "Hello Kitty";
+	}
+
+	@Override
+	public float getPlayTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isPlaying() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void start() throws JukeBoxException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
