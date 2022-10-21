5d865fac19f8792b08e8652b26ff0769c299cd4f
heshamouda <hesham.ouda@outlook.com>
2022-10-21 17:45:15 +0200

    create display method

---
+++ LibraryManagement/src/com.library.bean/User.java
@@ -0,0 +1,44 @@
+package com.library.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String userId;
+    private String name;
+    private String address;
+    private String bookName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+}
