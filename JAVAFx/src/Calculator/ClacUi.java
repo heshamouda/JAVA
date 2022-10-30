80be7b9cc4a7865616ec2658c2c2fd2a965d41e2
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-30 14:17:51 +0100

    complete the layout method

--- JAVAFx/src/Calculator/ClacUi.java
+++ JAVAFx/src/Calculator/ClacUi.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 
 public class ClacUi extends GridPane {
 
@@ -46,6 +50,38 @@ public class ClacUi extends GridPane {
     }
 
     private void layoutControls() {
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setHgrow(Priority.ALWAYS);
+        getColumnConstraints().addAll(cc, cc, cc, cc);
+
+        RowConstraints rc = new RowConstraints();
+        rc.setVgrow(Priority.ALWAYS);
+        getRowConstraints().addAll(new RowConstraints(), rc, rc, rc, rc, rc);
+
+        add(display, 0, 0, 4, 1);
+
+        add(ac, 0, 1);
+        add(plusMinus, 1, 1);
+        add(divide, 2, 1);
+        add(multiply, 3, 1);
+
+        add(numberButtons.get(7), 0, 2);
+        add(numberButtons.get(8), 1, 2);
+        add(numberButtons.get(9), 2, 2);
+        add(minus, 3, 2);
+
+        add(numberButtons.get(4), 0, 3);
+        add(numberButtons.get(5), 1, 3);
+        add(numberButtons.get(6), 2, 3);
+        add(plus, 3, 3);
+
+        add(numberButtons.get(1), 0, 4);
+        add(numberButtons.get(2), 1, 4);
+        add(numberButtons.get(3), 2, 4);
+        add(equal, 3, 4, 1, 2);
+
+        add(numberButtons.get(0), 0, 5, 2, 1);
+        add(dot, 2, 5);
 
     }
 
