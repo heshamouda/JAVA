d88880f0211086a4833932545ffea9bcb5f51d25
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-30 18:18:04 +0100

    add Project to style buttons

---
+++ JAVAFx/src/ButtonStyling/StyledButton.java
@@ -0,0 +1,28 @@
+package ButtonStyling;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class StyledButton extends StackPane {
+	private Button button;
+
+	public StyledButton() {
+        initializeSelf();
+		initializeControls();
+		layoutControls();
+	}
+
+	private void initializeSelf(){
+        String stylesheet = getClass().getResource("style.css").toExternalForm();
+ 		getStylesheets().add(stylesheet);
+    }
+
+	private void initializeControls() {
+		button = new Button("Hello World");
+	}
+
+	private void layoutControls() {
+		getChildren().add(button);
+	}
+
+}
