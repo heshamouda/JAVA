51ef6b8fda54038aec0bdf79c0913c4f0b3d0866
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-30 14:02:37 +0100

    create new app calculator and set its buttons

---
+++ JAVAFx/src/Calculator/ClacUi.java
@@ -0,0 +1,61 @@
+package Calculator;
+
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClacUi extends GridPane {
+
+    private ArrayList<Button> numberButtons = new ArrayList<>();
+
+    private Button dot;
+    private Button minus;
+    private Button plus;
+    private Button divide;
+    private Button multiply;
+
+    private Button plusMinus;
+    private Button equal;
+    private Button ac;
+
+    private Label display;
+
+    public ClacUi() {
+        intializeControls();
+        layoutControls();
+    }
+
+    private void layoutControls() {
+        for (int i = 0; i < 10; i++) {
+
+            numberButtons.add(createButton(String.valueOf(i)));
+
+            dot = createButton(".");
+            plus = createButton("+");
+            minus = createButton("-");
+            multiply = createButton("*");
+            divide = createButton("/");
+
+            plusMinus = createButton("+/-");
+            equal = createButton("=");
+            ac = createButton("C");
+
+            display = new Label("0");
+            display.setMaxWidth(Double.MAX_VALUE);
+
+        }
+    }
+
+    private Button createButton(String textString) {
+        Button btn = new Button(textString);
+        btn.setMaxWidth(Double.MAX_VALUE);
+        btn.setMaxHeight(Double.MAX_VALUE);
+
+        return btn;
+    }
+
+    private void intializeControls() {
+    }
+
+}
