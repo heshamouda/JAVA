ea5d5f9491dcb118e0bbbac6c2e41bcd715fd4ca
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-30 14:20:20 +0100

    create class CalcApp to start application of calc

---
+++ JAVAFx/src/Calculator/CalcApp.java
@@ -0,0 +1,26 @@
+package Calculator;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalcApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent rootPanel = new ClacUi();
+
+        Scene scene = new Scene(rootPanel);
+
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
